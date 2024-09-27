46ece99b910fec44020a3fc846ea09b5d7d79ace
DravZ <cdgo345@gmail.com>
2024-09-26 21:28:52 -0600

    Categorias v.2

--- src/components/Pages/postres/PostresComponent.tsx
+++ src/components/Pages/postres/PostresComponent.tsx
@@ -5,9 +5,14 @@ function PostresComponent(){
     return(
         <>
         <div className='conteiner-fluid'>
-            <div className='row justify-content-center m-0'>
-                <div className='col-11'>
-                    <CategoriasComponent categoria='Postres'/>
+            <div className='conteiner-rel'>
+                <div className='row mx-0 p-0'>
+                    <img className='p-0' src="../../../../public/img/Categorias/banner_pastel_choco.jpg" alt="" />
+                </div>
+                <div className='row justify-content-center m-0'>
+                    <div className='col-11 container-category'>
+                        <CategoriasComponent categoria='Postres'/>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/category/CardProductComponent/CardProductComponent.tsx
+++ src/components/category/CardProductComponent/CardProductComponent.tsx
@@ -6,12 +6,14 @@ function CardProductComponent(){
         <>
         <div className='col-3 px-3'>
             <div className="card col-12 my-2 card-product">
-                <div className="square-img">
-                    <img src="../../../public/EasterEgg.jpg" alt="Imagen de ejemplo"/>
-                </div>
-                <div className="card-body">
-                    <h4 className="card-title font-bold text-center">Chorizo con Huevos</h4>
-                </div>
+                <a href="" className='no-decoration fontclr-black'>
+                    <div className="square-img">
+                        <img src="../../../public/EasterEgg.jpg" alt="Imagen de ejemplo"/>
+                    </div>
+                    <div className="card-body">
+                        <h4 className="card-title font-bold text-center">Chorizo con Huevos</h4>
+                    </div>
+                </a>
             </div>
         </div>
         </>
