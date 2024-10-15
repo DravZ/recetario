e5183515d7f76103001b701af9cca7756191c936
DravZ <cdgo345@gmail.com>
2024-10-15 08:45:03 -0600

    index responsive

--- src/components/home/homeComponent/HomeComponent.tsx
+++ src/components/home/homeComponent/HomeComponent.tsx
@@ -11,7 +11,7 @@ function HomeComponent() {
         <MenuComponent/>
         <hr  className="mb-4"/>
         <div className="row d-flex align-content-center justify-content-center" >
-          <div className={`col-11 mt-4 ${styles.ojo}`}> 
+          <div className={`col-11 mt-4 d-none d-xl-block ${styles.ojo}`}> 
             <div className="row">
               <div className="col-6 mt-4 p-0">
                 <div
@@ -75,6 +75,69 @@ function HomeComponent() {
                 <div
                   className={`${styles.img_index}`} style={{ backgroundImage: `url(https://img.freepik.com/foto-gratis/madre-e-hija-cocinando-cocina-casa_23-2148805020.jpg)` }}
                 ></div>
+              </div>
+            </div>
+          </div>
+          <div className={`col-11 mt-4 d-block d-xl-none`}> 
+            <div className="row">
+              <div className="col-12 mt-4 p-0 d-flex justify-content-center">
+                <img className={`${styles.img_lg}`} src="https://resizer.otstatic.com/v2/photos/wide-huge/2/50470822.jpg" alt="" />
+              </div>
+              <div className="col-12 mx-0 py-0">
+                <div className={`row ${styles.div_text} text-fluid d-flex align-content-center`}>
+                  <div className="my-4 py-4">
+                    <h1 className="text-center">Esto es flavor fusion</h1>
+                    <h5>
+                      En Flavor Fusion, hemos creado un concepto único donde los sabores
+                      del mundo se encuentran en un solo lugar. Nuestro restaurante
+                      ofrece una fusión ecléctica de platos, con recetas inspiradas en
+                      diversas culturas y cocinas globales. Ya sea que tengas antojo de
+                      un suculento corte de carne, un delicioso sushi, una pasta casera,
+                      o un postre exótico, en <b>Flavor Fusion</b> lo tenemos todo.
+                      <br/><br/>
+                      Nuestro objetivo es que cada bocado te lleve en un viaje
+                      gastronómico, combinando ingredientes frescos, técnicas
+                      tradicionales y un toque moderno. Nos especializamos en crear
+                      experiencias inolvidables para el paladar, asegurando que siempre
+                      encuentres algo nuevo y emocionante en nuestro menú.
+                      <br/><br/>
+                      ¡Ven a descubrir el verdadero significado de la fusión de sabores
+                      en Flavor Fusion, donde la diversidad culinaria se celebra en cada
+                      plato!
+                      <br /><br />
+                    </h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12 p-0 d-flex justify-content-center">
+                <img className={`${styles.img_lg}`} src="https://img.freepik.com/foto-gratis/madre-e-hija-cocinando-cocina-casa_23-2148805020.jpg" alt="" />
+              </div>
+              <div className={`col-12 ${styles.div_text}`}>
+                <div className="my-4 ">
+                  <br></br>
+                  <h1 className="text-center">Nuestra misión</h1>
+                  <h5>
+                    En <b>Flavor Fusion</b>, nuestra misión es ofrecer a nuestros
+                    comensales una experiencia gastronómica única, combinando lo mejor
+                    de las cocinas del mundo en cada plato. Nos dedicamos a crear
+                    sabores innovadores que despierten los sentidos, utilizando
+                    ingredientes frescos y de la más alta calidad.
+                    <br></br> <br></br>
+                    Nos esforzamos por brindar un ambiente acogedor y moderno, donde
+                    cada cliente se sienta como en casa y pueda disfrutar de una
+                    aventura culinaria inolvidable. Creemos que la comida es una forma
+                    de conectar culturas y personas, y nos comprometemos a sorprender
+                    y deleitar a nuestros visitantes con cada bocado.
+                    <br></br> <br></br>
+                    En <b>Flavor Fusion</b>, nuestro objetivo es fusionar el mundo en
+                    tu plato, creando momentos especiales que reflejen la diversidad y
+                    la riqueza de la gastronomía global.
+                    <br />
+                    <br />
+                  </h5>
+                </div>
               </div>
             </div>
           </div>
--- src/components/home/menuComponent/MenuComponent.tsx
+++ src/components/home/menuComponent/MenuComponent.tsx
@@ -7,35 +7,35 @@ function MenuComponent() {
     return (
         <div className='container-fluid my-4 py-4'>
             <div className='row justify-content-center'>
-                <div className='col-11 my-1 py-4'>
+                <div className='col-11 my-1 py-sm-1 py-md-2 py-lg-3 py-xl-4 py-0'>
                     <div className='row'>
                         {/* Menú de enlaces */}
-                        <div className='col-2 mt-4 pt-4 px-0 d-flex justify-content-center align-items-center' style={{ height: '70vh' }}>
+                        <div className={`col-2 mt-0 mt-sm-4 mt-lg-4 pt-4 pt-md-3 pt-lg-4 px-0 d-flex justify-content-center align-items-center ${styles.menu_height}`}>
                             <Link href="/postres" className={`${styles.link_cont_men} d-flex justify-content-center align-items-center`}>
                                 <ItemMenuComponent img='/img/Categorias/postres/img_recetas/206-pastel-de-tres-leches.jpg' categoria='Postres' />
                             </Link>
                         </div>
-                        <div className='col-2 mb-4 pb-4 px-0 d-flex justify-content-center align-items-center' style={{ height: '70vh' }}>
+                        <div className={`col-2 mb-0 mb-sm-4 mb-lg-4 pb-4 pb-md-3 pb-lg-4 px-0 d-flex justify-content-center align-items-center  ${styles.menu_height}`}>
                             <Link href="/ensaladas" className={`${styles.link_cont_men} d-flex justify-content-center align-items-center`}>
                                 <ItemMenuComponent img='/img/Categorias/ensaladas/ensalada-quesos.jpg' categoria='Ensaladas' />
                             </Link>
                         </div>
-                        <div className='col-2 mt-4 pt-4 px-0 d-flex justify-content-center align-items-center' style={{ height: '70vh' }}>
+                        <div className={`col-2 mt-0 mt-sm-4 mt-lg-4 pt-4 pt-md-3 pt-lg-4 px-0 d-flex justify-content-center align-items-center ${styles.menu_height}`}>
                             <Link href="/pastas" className={`${styles.link_cont_men} d-flex justify-content-center align-items-center`}>
                                 <ItemMenuComponent img='/img/Categorias/pastas/pasta-lasana.jpg' categoria='Pastas' />
                             </Link>
                         </div>
-                        <div className='col-2 mb-4 pb-4 px-0 d-flex justify-content-center align-items-center' style={{ height: '70vh' }}>
+                        <div className={`col-2 mb-0 mb-sm-4 mb-lg-4 pb-4 pb-md-3 pb-lg-4 px-0 d-flex justify-content-center align-items-center  ${styles.menu_height}`}>
                             <Link href="/desayunos" className={`${styles.link_cont_men} d-flex justify-content-center align-items-center`}>
                                 <ItemMenuComponent img='/img/Categorias/desayunos/desayuno-panTiramisu.jpg' categoria='Desayunos' />
                             </Link>
                         </div>
-                        <div className='col-2 mt-4 pt-4 px-0 d-flex justify-content-center align-items-center' style={{ height: '70vh' }}>
+                        <div className={`col-2 mt-0 mt-sm-4 mt-lg-4 pt-4 pt-md-3 pt-lg-4 px-0 d-flex justify-content-center align-items-center ${styles.menu_height}`}>
                             <Link href="/ninos" className={`${styles.link_cont_men} d-flex justify-content-center align-items-center`}>
                                 <ItemMenuComponent img='/img/Categorias/ninos/nino-tiraPollo.jpg' categoria='Niños' />
                             </Link>
                         </div>
-                        <div className='col-2 mb-4 pb-4 px-0 d-flex justify-content-center align-items-center' style={{ height: '70vh' }}>
+                        <div className={`col-2 mb-0 mb-sm-4 mb-lg-4 pb-4 pb-md-3 pb-lg-4 px-0 d-flex justify-content-center align-items-center  ${styles.menu_height}`}>
                             <Link href="/dylan" className={`${styles.link_cont_men} d-flex justify-content-center align-items-center`}>
                                 <ItemMenuComponent img='/img/EasterEgg.jpg' categoria='Dylan' />
                             </Link>
